55ffabc4a1ebbe0fe1a27db468da343fee3b3909
emadkeyvani <emadkeyvani86@gmail.com>
2022-11-26 00:35:35 +0200

    fix bug on Favorites Fragment

--- app/src/main/java/com/ezatpanah/themealdb_api_mvp/ui/favorites/FavoritesPresenter.kt
+++ app/src/main/java/com/ezatpanah/themealdb_api_mvp/ui/favorites/FavoritesPresenter.kt
@@ -6,16 +6,20 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
-class FavoritesPresenter @Inject constructor(private val repository: DbRepository, val view: FavoritesContracts.View) :
-    BasePresenterImpl(), FavoritesContracts.Presenter {
+class FavoritesPresenter
+@Inject constructor(
+    private val repository: DbRepository,
+    val view: FavoritesContracts.View
+) : FavoritesContracts.Presenter, BasePresenterImpl() {
 
-    override fun loadAllFood() {
-        disposable = repository.loadAllFoods()
+    override fun getAllFood() {
+        disposable = repository
+            .loadAllFoods()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
                 if (it.isNotEmpty()) {
-                    view.showAllFoods(it)
+                    view.loadAllFoods(it)
                 } else {
                     //Empty
                 }
