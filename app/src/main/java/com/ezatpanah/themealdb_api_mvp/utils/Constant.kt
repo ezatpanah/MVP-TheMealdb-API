86c0eaad615a0e9f849c420d99d81940658400f3
Reyhaneh Ezatpanah <ezatpanah@gmail.com>
2022-11-27 02:26:21 +0200

    Create Constant.kt

---
+++ app/src/main/java/com/ezatpanah/themealdb_api_mvp/utils/Constant.kt
@@ -0,0 +1,14 @@
+package com.ezatpanah.themealdb_api_mvp.utils
+
+object Constant {
+
+    const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+    const val NETWORK_TIMEOUT = 60L
+
+    const val YOUTUBE_API_KEY = "***"
+    const val VIDEO_ID = "video_id"
+
+    const val FOOD_TABLE = "food_table"
+    const val FOOD_DATABASE = "food_database"
+
+}
